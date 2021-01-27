e7d4dedbca683f2c54646a70b96521c4f9895af3
Lucas <lukymassaro@gmail.com>
2021-01-27 13:43:06 -0300

    animate hamburguer btn on click

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,10 @@
-console.log('Hola mundo')
+const hamburguer = document.querySelector('.header__hamburger');
+console.log(hamburguer);
+hamburguer.addEventListener('click', function() {
+    console.log('click en el menu');
+    if (this.classList.contains('open')) {
+        this.classList.remove('open');
+    } else {
+        this.classList.add('open');
+    }
+});
