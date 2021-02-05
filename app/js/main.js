005607be9b1ac7ceb323a43760cb20e7d7279cb6
Lucas <lukymassaro@gmail.com>
2021-02-05 08:22:56 -0300

    animaciones nav

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,23 @@
 const hamburguer = document.querySelector('.header__hamburger');
-console.log(hamburguer);
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
 hamburguer.addEventListener('click', function() {
-    console.log('click en el menu');
-    if (this.classList.contains('open')) {
-        this.classList.remove('open');
-    } else {
-        this.classList.add('open');
+    if (header.classList.contains('open')) { // Close
+        header.classList.remove('open');
+        body.classList.remove('no-scroll');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    } else { // Open
+        header.classList.add('open');
+        body.classList.add('no-scroll');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
     }
 });
